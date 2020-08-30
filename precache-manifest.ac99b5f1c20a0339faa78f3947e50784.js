29139d69fc5361e474224ea8659d8078a036d0a6
TanimaRanjan <tanimaranjan@gmail.com>
2020-08-29 22:16:01 -0400

    Updates

---
+++ precache-manifest.ac99b5f1c20a0339faa78f3947e50784.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4970a915dbf76b4c2312f36a449667a7",
+    "url": "/react-carousel.github.io/index.html"
+  },
+  {
+    "revision": "c9b50639fb3915c41d30",
+    "url": "/react-carousel.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7eebf530a34790e728dd",
+    "url": "/react-carousel.github.io/static/js/2.b721051e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-carousel.github.io/static/js/2.b721051e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9b50639fb3915c41d30",
+    "url": "/react-carousel.github.io/static/js/main.f763fb65.chunk.js"
+  },
+  {
+    "revision": "e9f0ea338e736ac7666b",
+    "url": "/react-carousel.github.io/static/js/runtime-main.0f6ffcdc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-carousel.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
