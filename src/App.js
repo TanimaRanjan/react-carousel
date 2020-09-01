d73fbc70bd66c9d183fd9c99e2a0fbc7d2f4bd10
TanimaRanjan <tanimaranjan@gmail.com>
2020-09-01 07:38:16 -0400

    Adding Carousel

--- src/App.js
+++ src/App.js
@@ -3,11 +3,39 @@ import Header from './Header';
 import Footer from './Footer';
 import Carousel from './Carousel';
 
+const slides = ["https://images.unsplash.com/photo-1499002238440-d264edd596ec?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+"https://images.unsplash.com/photo-1532996228489-082708ddbde0?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+"https://images.unsplash.com/photo-1530625243345-797b4c1836ee?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+"https://images.unsplash.com/photo-1566545455366-bcae28fd3929?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+"https://images.unsplash.com/photo-1566305828756-cacc74d3dd20?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+"https://images.unsplash.com/photo-1482005253821-5d6a2c685879?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+"https://images.unsplash.com/photo-1555454762-24a52b98f75f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+"https://images.unsplash.com/photo-1565378435245-4282cbde883e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+"https://images.unsplash.com/photo-1565378434747-262417385c7f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+"https://images.unsplash.com/photo-1497290756760-23ac55edf36f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+"https://images.unsplash.com/photo-1465189684280-6a8fa9b19a7a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+"https://images.unsplash.com/photo-1483425571841-9662f86c7154?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+"https://images.unsplash.com/photo-1563427777882-8c6e10fa4920?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+"https://images.unsplash.com/photo-1560815633-37d240814f3d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+"https://images.unsplash.com/photo-1490682143684-14369e18dce8?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9", 
+"https://images.unsplash.com/photo-1561363702-e07252da3399?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+"https://images.unsplash.com/photo-1562337589-136d8a7dd9dd?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9", 
+"https://images.unsplash.com/photo-1462275646964-a0e3386b89fa?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9", 
+"https://images.unsplash.com/photo-1558999539-7a19738f085d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9", 
+"https://images.unsplash.com/photo-1560241692-f4f0432ced72?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9", 
+"https://images.unsplash.com/photo-1559626627-cb31b201e27f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9", 
+"https://images.unsplash.com/photo-1557474295-714ea5fc2557?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+"https://images.unsplash.com/photo-1553082662-5f1a1f3cc83b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+"https://images.unsplash.com/photo-1554185256-7b994c659b88?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+"https://images.unsplash.com/photo-1557502706-5a0e03129173?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9" ]
+
+
+
 function App() {
   return (
     <main>
       <Header />
-      <Carousel />
+      <Carousel slides={slides}/>
       <Footer />
     </main>
   );
