846b1ec96f8fc0bce7da36aabac70c3a042eb109
TanimaRanjan <tanimaranjan@gmail.com>
2020-09-03 20:45:28 -0400

    Carousel button

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import { useEmblaCarousel } from 'embla-carousel/react'
 
-const Carousel = () => {
+const Carousel = ({slides}) => {
+
+    const [viewportRef, embla] = useEmblaCarousel()
 
 
     return (
-        <div>Show slides</div>
+        <div className="embla">
+            <div className="embla__viewport" ref={viewportRef}>
+                <div className="embla__container">
+                    {slides.map((slide, index) => 
+                        <div className="embla__slide" >
+                            <div className="embla__slide__inner" key={index}>
+                                <img
+                                    className="embla__slide__img"
+                                    src={slide}
+                                    alt="A cool view"
+                                />
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
     )
 }
 
